3415cdc41406f3c807972c82017e6c73ca69b678
josie-shie <joe003218@gmail.com>
2021-05-07 16:57:46 +0800

    5/7 class

--- museum/login.php
+++ museum/login.php
@@ -1,43 +1,46 @@
 <?php
-//引用資料庫連線
-require_once('db.inc.php');
+// 啟動 session
+session_start();
 
-if( isset($_POST['userName']) && isset($_POST['pwd']) ){
-    //SQL 語法
-    $sql = "SELECT `userName`, `pwd` 
+// 引用資料庫連線
+// require_once會讀取或跳轉到括弧內網址一次,也可以寫成 require_once './db.inc.php'
+require_once('./db.inc.php');
+
+if (isset($_POST['username']) && isset($_POST['pwd'])) {
+    // SQL 語法,?讓pdo處理後續程式(data finding 資料細節)
+    $sql = "SELECT `username`, `pwd` 
             FROM `admin` 
-            WHERE `userName` = ? 
+            WHERE `username` = ? 
             AND `pwd` = ? ";
 
     $arrParam = [
-        $_POST['userName'],sha1($_POST['pwd'])
+        $_POST['username'],
+        sha1($_POST['pwd'])
     ];
-    //跟資料庫有關都需要用到$pdo
-    $pdo_stmt = $pdo->prepare($sql); //製作資料細節 將/帶出資料阻擋SQL Injection
-    $pdo_stmt->execute($arrParam);//將加完反斜線的資料($arrParam)帶入$pdo_stmt
 
-    if( $pdo_stmt->rowCount() > 0 ){ 
-        //->物件導向 表示使用pdo的物件屬性(想當於js的.)
-        //rowCount()計算資料庫中的影響列表
-        //將傳送過來的 post 變數資料，放到 session，
-        $_SESSION['userName'] = $_POST['userName'];
+    // $pdo_stmt 存放資料細節
+    // ->是php的物件導向(使用物件的方法)近似於JS的'.'
+    $pdo_stmt = $pdo->prepare($sql); // prepare 新增跳脫字元讓之後程式判讀特殊字元為字串
+    $pdo_stmt->execute($arrParam); // execute 執行前面prepare模擬好的狀態
 
-        //3 秒後跳頁
-        header("Refresh: 9; url=./musAdmin.php");
-        echo '登入成功';
-        //require_once('templates/login_success.html');//跳轉到這個頁面
+    if ($pdo_stmt->rowCount() > 0) {
+        // 將傳送過來的 post 變數資料，放到 session，
+        $_SESSION['username'] = $_POST['username'];
+
+        // 3秒後跳頁
+        header("Refresh: 1; url=./musList.php");
+        echo "登入成功!!! 1秒後自動進入後端頁面";
     } else {
-        //關閉 session
+        // 關閉session
         session_destroy();
 
-        header("Refresh: 3; url=./loginpage.php");
-        echo '登入失敗';
-        //require_once('templates/login_failed.html');
+        header("Refresh: 3; url=./index.php");
+        echo "登入失敗…3秒後自動回登入頁";
     }
 } else {
-    //關閉 session
+    // 關閉session
     session_destroy();
 
-    header("Refresh: 3; url=./loginpage.php");
+    header("Refresh: 3; url=./index.php");
     echo "請確實登入…3秒後自動回登入頁";
 }
--- museum/templates/title.php
+++
@@ -1,2 +0,0 @@
-台北市美術館總覽 - <a href="./musAdmin.php">美術館全覽</a> | <a href="./musAdd.php">新增館別</a> | <a href="./logout.php?logout=1">登出</a>
-<hr />
