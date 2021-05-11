8bdd5da03441730e986fb6a6295f51ffaf33827b
josie-shie <joe003218@gmail.com>
2021-05-11 13:56:38 +0800

    5/10 class

--- museum/registerPage/itemsPage/myCart.php
+++ museum/registerPage/itemsPage/myCart.php
@@ -15,6 +15,15 @@ require_once '../db.inc.php';
         <!-- Core theme CSS (includes Bootstrap)-->
         <link href="css/styles.css" rel="stylesheet" />
         <style>
+            html,body{
+                height: 100%
+            }
+            .main-content{
+            min-height: 100%;
+            margin: 0 auto;
+            padding-top:50px;
+            padding-bottom:-50px;
+            }
             .btncolor{
                 background:#1D0AFF;
             }
@@ -28,6 +37,12 @@ require_once '../db.inc.php';
             margin: 10px;
             border: 1px solid #aaa;
             background: #f8f8f8;
+            }
+
+            .footer{
+            position: relative;
+            top: -100px;
+            height: 50px;
             }
 
         </style>
@@ -65,13 +80,94 @@ require_once '../db.inc.php';
                 </div>
             </div>
         </nav>
-                            
+
+        <div class="col-md-10 col-sm-9 main-content">
+            <div class="row pl-3 pr-3">
+                <div class="table-responsive">
+                    <table class="table table-sm">
+                        <thead>
+                            <tr>
+                                <th scope="col" class="border-0 bg-light">
+                                    <div class="p-2 px-3 text-uppercase">商品名稱</div>
+                                </th>
+                                <th scope="col" class="border-0 bg-light">
+                                    <div class="py-2 text-uppercase">價格</div>
+                                </th>
+                                <th scope="col" class="border-0 bg-light">
+                                    <div class="py-2 text-uppercase">數量</div>
+                                </th>
+                                <th scope="col" class="border-0 bg-light">
+                                    <div class="py-2 text-uppercase">小計</div>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $arr=[];
+                            $total=0;
+                            if(isset($_SESSION["cart"]) && count($_SESSION["cart"])> 0){
+                                $_SESSION["cart"] = array_values($_SESSION["cart"]);
+                                $sql="SELECT `itemId`, `itemName`, `itemImage`, `itemPrice`,`itemQty`
+                                    FROM`items`
+                                    WHERE `itemId`=?";
+                                    for($i = 0; $i < count($_SESSION["cart"]); $i++){
+                                        $arrParam = [
+                                            (int)$_SESSION["cart"][$i]["itemId"]
+                                        ];
+                                        $stmt = $pdo->prepare($sql);
+                                        $stmt->execute($arrParam);
+                                    if($stmt->rowCount() > 0) {
+                                        $arrTmp = $stmt->fetchAll(PDO::FETCH_ASSOC)[0];
+                                        $arrTmp['cartQty'] = $_SESSION["cart"][$i]["cartQty"];
+                                        $arr[] = $arrTmp;
+                                    }
+                                }
+                            }
+                            for($i = 0; $i < count($arr); $i++) { 
+                                //計算總額
+                                $total += $arr[$i]["itemPrice"] * $arr[$i]["cartQty"];?>
+                                <tr>
+                                    <th scope="row" class="border-0">
+                                        <div class="p-2">
+                                            <img src="./images/<?php echo $arr[$i]["itemImage"]?>"alt="" width="70" class="img-fluid rounded shadow-sm">
+                                            <div class="ml-3 d-inline-block align-middle">
+                                                <h5 class="mb-0"><a href="#"class="text-dark d-inline-block align-middle"><?php echo $arr[$i]["itemName"] ?></a></h5>
+                                            </div>
+                                        </div>
+                                    </th>
+                                    <td class="border-0 align-middle"><strong>$<?php echo $arr[$i]["itemPrice"] ?></strong></td>
+                                    <td class="border-0 align-middle">
+                                        <input type="text" class="form-control" name="cartQty[]" value="<?php echo $arr[$i]["cartQty"] ?>" maxlength="3">
+                                    </td>
+                                    <td class="border-0 align-middle">
+                                        <input type="text" class="form-control" name="subtotal[]" value="<?php echo ($arr[$i]["itemPrice"] * $arr[$i]["cartQty"]) ?>" maxlength="10">
+                                    </td>
+                                    <td class="border-0 align-middle"><a href="./deleteCart.php?idx=<?php echo $i ?>" class="text-dark">刪除</a>
+                                    </td>
+                                </tr>
+                                <input type="hidden" name="itemId[]" value="<?php echo $arr[$i]["itemId"] ?>">
+                                <input type="hidden" name="itemPrice[]" value="<?php echo $arr[$i]["itemPrice"] ?>">
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>                    
+                                    
+
+
+                        
+                        
 
 
                             
+
+
                 
         <!-- Footer-->
-        <footer class="py-5 bg-dark">
+        <footer class="py-5 bg-dark footer">
             <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
         </footer>
         <!-- Bootstrap core JS-->
